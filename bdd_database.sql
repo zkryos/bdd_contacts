c76559a43b087d2da9a37a0f4d4a2df981f459e0
Zekariya Bektas <zekariya@MacBook-Air-de-Zekariya.local>
2023-11-23 09:41:07 +0100

    Mise en place de la table iso_3

--- bdd_database.sql
+++ bdd_database.sql
@@ -2,3 +2,9 @@ DROP DATABASE if EXISTS bdd_contact.sql;
 CREATE DATABASE bdd_contact_sql CHARACTER set = 'utf8' COLLATE = 'utf8_general_ci'; 
 
 USE bdd_contact_sql; 
+
+CREATE table pays_iso_3 (
+    alpha_3 CHAR(3) PRIMARY KEY, 
+    nom VARCHAR (50)
+); 
+
