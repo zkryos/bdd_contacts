c35396bcb1413d1d76ba458e1a16cea08de8dc49
Zekariya Bektas <zekariya@MacBook-Air-de-Zekariya.local>
2023-11-23 10:08:02 +0100

    Correction - Creation de la table pays avec les ISO

--- bdd_database.sql
+++ bdd_database.sql
@@ -3,36 +3,12 @@ CREATE DATABASE bdd_contact_sql CHARACTER set = 'utf8' COLLATE = 'utf8_general_c
 
 USE bdd_contact_sql; 
 
-CREATE table pays_iso_3 (
-    alpha_3 CHAR(3) PRIMARY KEY, 
-    nom VARCHAR (50)
-    alpha_2 CHAR(3) 
-); 
-
-INSERT INTO pays_iso_3 (alpha_3, nom) VALUES
-    ('FRA', 'FRANCE'),
-    ('SUI', 'SUISSE'),
-    ('TR', 'TURQUIE'),
-    ('BE', 'BELGIQUE'); 
-
-CREATE table contacts ( 
- id int PRIMARY KEY AUTO_INCREMENT, 
- nom VARCHAR(70), 
- prenom VARCHAR(70),
- date_de_naisssance DATE, 
- sexe VARCHAR(10), 
- adresse TEXT,
- cp VARCHAR(15), 
- ville VARCHAR(70), 
-FOREIGN KEY (pays) REFERENCES pays_iso_3(alpha_3) 
-); 
-
-CREATE table telephone ( 
- id int PRIMARY KEY AUTO_INCREMENT, 
- id_contact int  AUTO_INCREMENT,
- numero VARCHAR(40), 
- type TINYINT
-); 
+CREATE TABLE pays (
+    iso_3 CHAR(3) PRIMARY KEY, 
+    nom VARCHAR(70),
+    iso_2 CHAR(2), 
+    nationalite VARCHAR(50)
+);
 
 
 
